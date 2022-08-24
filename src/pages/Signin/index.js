1924571126fa69bbc5c87a5bcb01984c1c53efba
ANDERSON DA SILVA FERNANDES <64322982+Anderson2022@users.noreply.github.com>
2022-08-24 14:46:47 -0400

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   package-lock.json
    	renamed:    src/hooks/userAuth.js -> src/hooks/useAuth.js
    	modified:   src/pages/Signin/index.js
    	modified:   src/pages/Signup/styles.js
    	modified:   yarn.lock

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -3,14 +3,37 @@ import Input from '../../components/input';
 import Button from '../../components/button';
 import * as C from './style';
 import {Link, useNavigate} from "react-router-dom";
-import useAuth from '../../hooks/userauth';
+import useAuth from '../../hooks/useuth';
+
+/*===================== confirmação do login===========*/
+
 
 
 const Signin = () => {
+        const { signin} =useAuth();
+        const navigate = useNavigate();
+
 
         const[email, setEmail] = useState("");
         const[senha, setSenha] = useState("");
         const[error, setError] = useState("");
+
+        const handleLogin = () => {
+          if (!email | !senha) {
+            setError("Preencha todos os campos");
+            return;
+          }
+
+          const res = signin(email, senha);
+
+            if (res) {
+              setError(res);
+              return;
+            }
+
+            navigate("/home");
+        };    
+
 
   return (
     <C.Container>
@@ -29,11 +52,15 @@ const Signin = () => {
                 onChange={(e) => [setSenha(e.target.value), setError("")]}
                 />
                 <C.labelError>{error}</C.labelError>
-                <Button Text={Entrar} onClick={handleLogin}/>
-                <C.Strong>
-                    <link to="/signup">&nbsp; Registre-se</link>
-                </C.Strong>
-             <C.LabelSignup>
+                <Button Text="Entrar" onClick={handleLogin}/>
+                <C.LabelSignup>
+                  Não tem uma conta?
+                  <C.Strong>
+                      <link to="/signup">&nbsp; Registre-se</link>
+                  </C.Strong>        
+
+               </C.LabelSignup>
+               
            </C.Content>
     </C.Container>
   );
