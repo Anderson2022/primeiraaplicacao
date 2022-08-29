62662d910a55d50229784b502316e8f0814ceb39
ANDERSON DA SILVA FERNANDES <64322982+Anderson2022@users.noreply.github.com>
2022-08-29 13:00:25 -0400

    modified:   src/pages/Home/App.js

--- src/pages/Home/App.js
+++ src/pages/Home/App.js
@@ -1,28 +1,9 @@
 import './App.css';
-import Header from './components/Header';
- default Home;
-import React from 'react';
-import './App.css';
-import Navbar from './components/Sidebar/NavBar';
-import { BrowserRouter , Switch, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Reports from './pages/Reports';
-import Products from './pages/Products';
-import Header from './components/Header';
+import Header from './components/Header'
 
 function App() {
   return (
-    <>
-     <Header />
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/reports' component={Reports} />
-          <Route path='/products' component={Products} />
-        </Switch>
-      </BrowserRouter>
-    </>
+    <Header />
   );
 }
 
