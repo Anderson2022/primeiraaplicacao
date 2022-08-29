7d8e0696331e368f9e562fff5b21d73d98436df3
ANDERSON DA SILVA FERNANDES <64322982+Anderson2022@users.noreply.github.com>
2022-08-29 15:42:08 -0400

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   src/pages/Home/components/Header/index.jsx
    	deleted:    src/pages/Home/components/Sidebar/NavBar.js
    	deleted:    src/pages/Home/components/Sidebar/SidebarData.js
    	deleted:    src/pages/Home/components/Sidebar/Sider.css
    	new file:   src/pages/Home/components/Sidebar/index.jsx
    	new file:   src/pages/Home/components/Sidebar/styles.js

---
+++ src/pages/Home/components/Sidebar/index.jsx
@@ -0,0 +1,52 @@
+import React, {useState} from 'react'
+import { Container, Content } from './styles'
+import { 
+  FaTimes, 
+  FaHome, 
+  FaEnvelope, 
+  FaRegSun, 
+  FaUserAlt, 
+  FaIdCardAlt, 
+  FaRegFileAlt,
+  FaRegCalendarAlt,
+  FaChartBar
+} from 'react-icons/fa'
+import useAuth from "../../../../hooks/useAuth"
+import SidebarItem from '../SidebarItem'
+import { useNavigate } from 'react-router-dom'
+import Button from "../../components/Button";
+
+
+const Sidebar = ({ active }) => {
+
+  const { signout } = useAuth();
+  const navigate = useNavigate();
+
+  const closeSidebar = () => {
+    active(false)
+  }
+
+
+
+  return (
+    <Container sidebar={active}>
+      <FaTimes onClick={closeSidebar} />  
+      <Content>
+        <SidebarItem Icon={FaHome} Text="Inicio" />
+        <SidebarItem Icon={FaChartBar} Text="Estatistica" />
+        <SidebarItem Icon={FaUserAlt} Text="Usuario" />
+        <SidebarItem Icon={FaEnvelope} Text="E-Mail" />
+        <SidebarItem Icon={FaRegCalendarAlt} Text="Calendario" />
+        <SidebarItem Icon={FaIdCardAlt} Text="Finanças" />
+        <SidebarItem Icon={FaRegFileAlt} Text="Relatorios" />
+        <SidebarItem Icon={FaRegSun} Text="Configurações" />
+          
+        <Button Text="Sair" onClick={() => [signout(), navigate("/")]}>
+        Sair
+      </Button>
+      </Content>
+    </Container>
+  )
+}
+
+export default Sidebar
